fa1bdea478f035f8b3d7faecbb8eb4d17946b093
Dan Koster <koster.dan@gmail.com>
2024-07-31 23:42:14 -0700

    clean up code and constrain position

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,11 +13,14 @@ signal spawned(player)
 @onready var explodeSound = $Explode
 @onready var laserSound = $Laser
 
+var laser_scene = preload("res://scenes/laser.tscn")
+
 var shotCooldown := false
 var shotRate := 0.15
 var isAlive := true
-
-var laser_scene = preload("res://scenes/laser.tscn")
+var target: PhysicsBody2D
+var elapsed := 1.0
+var tetherLength := 0.0
 
 func _process(delta):
 	if !isAlive: return
@@ -25,35 +28,24 @@ func _process(delta):
 	if Input.is_action_just_pressed("shoot"): 
 		$LaserBeam2D.is_casting = true
 		target = nearestNode(get_parent().asteroids.get_children())
-		print(str(Time.get_ticks_msec()) + " ---- LASER! ----> " + str(target))
-		$DampedSpringJoint2D.node_b = target.get_path()
+		tetherLength = global_position.distance_to(target.global_position)
+		print(str(Time.get_ticks_msec()) + " ---- LASER! ----> " + str(target.name) + " " + str(tetherLength))
+		#$DampedSpringJoint2D.node_b = target.get_path()
 
 	if Input.is_action_just_released("shoot"): 
 		$LaserBeam2D.is_casting = false
 		$DampedSpringJoint2D.node_b = ""
+		print(str(Time.get_ticks_msec()) + '------------------')
 		target = null
+	
+	if Input.is_action_pressed("rotate_right"):
+		rotate(deg_to_rad(rotation_speed * delta))
+	if Input.is_action_pressed("rotate_left"):
+		rotate(deg_to_rad(-rotation_speed * delta))
+	
+	if Input.is_anything_pressed():
+		elapsed = 0.0
 
-func shoot_laser(): 
-	assert(isAlive, "tried to shoot a laser while !isAlive")
-	var l = laser_scene.instantiate()
-	l.global_position = gun.global_position
-	l.rotation = rotation
-	laserSound.play()
-	laser_shot.emit(l)
-
-func nearestNode(nodes: Array[Node]) -> Node:
-	var nearest: Asteroid = null
-	var nearestDist: float
-	for a in nodes:
-		var d = global_position.distance_to(a.global_position)
-		if nearest == null || d < nearestDist: 
-			nearest = a
-			nearestDist = d
-	return nearest
-
-var target: PhysicsBody2D
-
-var elapsed := 1.0
 func _physics_process(delta):
 	
 	#TODO: wait until the player is moving tangental
@@ -65,27 +57,8 @@ func _physics_process(delta):
 		velocity += inputVector.rotated(rotation) * acceleration * delta
 		velocity = velocity.limit_length(maxSpeed)
 
-	if Input.is_action_pressed("rotate_right"):
-		rotate(deg_to_rad(rotation_speed * delta))
-	if Input.is_action_pressed("rotate_left"):
-		rotate(deg_to_rad(-rotation_speed * delta))
-	
-	if Input.is_anything_pressed():
-		elapsed = 0.0
-
-	#correct direction of travel to face velocity vector
-	if elapsed < 1:
-		var targetAngle = velocity.angle() + deg_to_rad(90)
-		rotation = lerp_angle(rotation, targetAngle, elapsed)
-		elapsed += delta * (velocity.length() * 0.001)
-		
-		var targetVector = Vector2.from_angle(rotation)
-		var vNorm = velocity.normalized()
-		var dot = vNorm.dot(targetVector)
-		var msec = Time.get_ticks_msec()
-		if(abs(dot) < 0.01):
-			print(str(msec) + " done correcting direction of travel:" + str(dot))
-			elapsed = 1.0
+	#correct totation to face velocity vector up
+	correctRotation(delta)
 		
 	var collision = move_and_collide(velocity)
 	if collision:
@@ -97,14 +70,61 @@ func _physics_process(delta):
 	#only need to change the target sometimes
 	#TODO: constrain motion https://code.tutsplus.com/swinging-physics-for-player-movement-as-seen-in-spider-man-2-and-energy-hook--gamedev-8782t
 	if $LaserBeam2D.is_casting:
-		target = nearestNode(get_parent().asteroids.get_children())
-		print(str(Time.get_ticks_msec()) + " ---- LASER! ----> " + str(target))
+		
+		#To find a vector pointing from A(target) to B(self), use B(self) - A(target)
+		#if (testPosition - tetherPoint ).Length() > tetherLength )
+		#{
+		#	// we're past the end of our rope 
+		#	// pull the avatar back in. 
+		#	testPosition = (testPosition - tetherPoint).Normalized() * tetherLength;
+		#}
+		
+		#constrain position
+		var targetToSelf = global_position - target.global_position
+		if targetToSelf.length() > tetherLength: 
+			var newPos = target.global_position + (targetToSelf.normalized() * tetherLength)
+			global_position = newPos
+		
 		$LaserBeam2D.look_at(target.global_position)
-		$DampedSpringJoint2D.look_at(target.global_position)
-		$DampedSpringJoint2D.node_b = target.get_path()
-	else:
-		target = null
-		$DampedSpringJoint2D.node_b = ""
+		#$DampedSpringJoint2D.look_at(target.global_position)
+		#$DampedSpringJoint2D.node_b = target.get_path()
+
+
+
+func shoot_laser(): 
+	assert(isAlive, "tried to shoot a laser while !isAlive")
+	var l = laser_scene.instantiate()
+	l.global_position = gun.global_position
+	l.rotation = rotation
+	laserSound.play()
+	laser_shot.emit(l)
+
+
+func nearestNode(nodes: Array[Node]) -> Node:
+	var nearest: Asteroid = null
+	var nearestDist: float
+	for a in nodes:
+		var d = global_position.distance_to(a.global_position)
+		if nearest == null || d < nearestDist: 
+			nearest = a
+			nearestDist = d
+	return nearest
+
+
+func correctRotation(delta): 
+	if elapsed < 1:
+		var targetAngle = velocity.angle() + deg_to_rad(90)
+		rotation = lerp_angle(rotation, targetAngle, elapsed)
+		elapsed += delta * (velocity.length() * 0.001)
+		
+		var targetVector = Vector2.from_angle(rotation)
+		var vNorm = velocity.normalized()
+		var dot = vNorm.dot(targetVector)
+		var msec = Time.get_ticks_msec()
+		if(abs(dot) < 0.01):
+			#print(str(msec) + " done correcting direction of travel:" + str(dot))
+			elapsed = 1.0
+
 
 func explode():
 	assert(isAlive, "tried to explode while !isAlive")
