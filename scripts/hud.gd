e61a6d4d9a08e2c0892567718f3739cdec3d5a38
Dan Koster <koster.dan@gmail.com>
2024-08-03 09:25:17 -0700

    remove logging

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -11,13 +11,10 @@ var uiLife = preload("res://scenes/ui_life.tscn")
 	get(): return livesContainer.get_child_count()
 	set(value): 
 		var count := livesContainer.get_child_count()
-		print('count: ' + str(count) + " value: " + str(value))
 		
 		while count > value: 
 			count -= 1
 			livesContainer.get_child(count).queue_free()
-			print('removed one')
 		while count < value:
 			count += 1
 			livesContainer.add_child(uiLife.instantiate())
-			print('added one')
