9ad9c95c7f0c0feaed4ce9308446bcbfa33a8ddd
Dan Koster <koster.dan@gmail.com>
2024-08-27 13:41:05 -0700

    tractor beam tuning

--- scenes/tractor_beam.gd
+++ scenes/tractor_beam.gd
@@ -4,21 +4,25 @@ var tetherLength := 0.0
 var source: PhysicsBody2D
 var target: PhysicsBody2D
 
+@export var pullStrength = 0.01
+
 func _ready():
 	source = get_parent()
 	assert(source is PhysicsBody2D)
+	$DampedSpringJoint2D.node_a = source.get_path()
 
 func setBeamTarget(node: PhysicsBody2D):
 	if (node):
 		target = node
+		tetherLength = source.global_position.distance_to(target.global_position)
 		$Line2D.add_point(source.global_position)
 		$Line2D.add_point(target.global_position)
-		tetherLength = source.global_position.distance_to(target.global_position)
+		$DampedSpringJoint2D.node_b = target.get_path()
 	else:
-		$DampedSpringJoint2D.node_b = ""
-		tetherLength = 0.0
 		target = null
+		tetherLength = 0.0
 		$Line2D.clear_points()
+		$DampedSpringJoint2D.node_b = ""
 
 func _physics_process(_delta):
 	if target:
@@ -36,14 +40,17 @@ func _physics_process(_delta):
 			
 			#rotate in the direction of travel
 			source.rotation = source.velocity.orthogonal().rotated(deg_to_rad(180)).angle()
-			
-			#TODO: replace spring with actual physics calculation to move the target
-			# $DampedSpringJoint2D.node_b = target.get_path()
+
+			#TODO: replace spring with force calculations for moving the target
+			$DampedSpringJoint2D.node_b = target.get_path()
+			# if target is RigidBody2D:
+			# 	target.apply_impulse()
+			# 	target.apply_torque_impulse()
 			
 		# add a little velocity as the tether is pulling in
 		# shorten the tether if we're approaching the target so we don't go past and bounce
 		elif length < tetherLength:
-			source.velocity += source.velocity * 0.01 
+			source.velocity += source.velocity * pullStrength 
 			tetherLength = length
 		
-		# $DampedSpringJoint2D.look_at(target.global_position)
+		$DampedSpringJoint2D.look_at(target.global_position)
